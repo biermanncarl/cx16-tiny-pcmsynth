f5dabfee287857defa03e1b11216b7bbe6e85333
biermanncarl <carlgeorg@gmx.net>
2020-10-11 12:02:12 +0200

    clean playback and return

--- pcm_synth.asm
+++ pcm_synth.asm
@@ -17,18 +17,33 @@
    ; make playback timed
    ; reduce latency
 
+LastSample:
+   .byte $00
+
 DefaultInterruptHandler:
    .word $0000
 
 MyInterruptHandler:
-@loop:
+   ; first check if interrupt is an AFLOW interrupt
+   lda VERA_isr
+   and #$08
+   beq @continue
+
    ; fill up FIFO buffer up to the top
+   ldx LastSample
+@loop:
    inx
    stx VERA_audio_data     ; and append it to the buffer
+   ; continue until buffer is full
    lda VERA_audio_ctrl     ; check if buffer is full
    and #$80
    beq @loop
-   jmp DefaultInterruptHandler
+
+   stx LastSample
+@continue:
+   ; call default interrupt handler
+   ; for keyboard service
+   jmp (DefaultInterruptHandler)
 
 
 start:
@@ -47,7 +62,6 @@ start:
    sta IRQVec+1
    cli            ; allow interrupts
 
-
    ; prepare playback
    lda #$8F       ; reset PCM buffer, 8 bit mono, max volume
    sta VERA_audio_ctrl
@@ -55,18 +69,25 @@ start:
    lda #0         ; set playback rate to zero
    sta VERA_audio_rate
    tax            ; initial audio sample
-   ; fill buffer once with zeroes
+   ; fill buffer once
    lda #0
    tax
 @loop:
+   inx
    stx VERA_audio_data     ; and append it to the buffer
    lda VERA_audio_ctrl     ; check if buffer is full
    and #$80
    beq @loop
+   stx LastSample
 
    ; start playback
    lda #128
    sta VERA_audio_rate
+
+   ; enable AFLOW interrupt
+   lda VERA_ien
+   ora #$08
+   sta VERA_ien
 
    ; main loop ... wait until "Q" is pressed. Playback is maintained by interrupts.
 mainloop:
@@ -75,17 +96,35 @@ mainloop:
    beq done
    ; cmp #65        ; branch if pressing "A"
    ; bne somewhere
+
+   lda LastSample
+   jsr CHROUT
+
    jmp mainloop
 
 
-
-
-
-playback:         ; start playback
-   lda #128
+done:
+   ; stop playback
+   lda #0
    sta VERA_audio_rate
 
-   jmp start
+   ; restore interrupt handler
+   sei            ; block interrupts
+   lda #<DefaultInterruptHandler
+   sta IRQVec
+   lda #>DefaultInterruptHandler
+   sta IRQVec+1
+   cli            ; allow interrupts
 
-done:
+   ; reset FIFO buffer
+   lda #$8F
+   sta VERA_audio_ctrl
+
+   ; disable AFLOW interrupt
+   lda VERA_ien
+   and #$F7
+   sta VERA_ien
+
    rts            ; return to BASIC
+   ; NOTE: the binary program will be destroyed once returned to BASIC
+   ; and needs to be loaded again in order to run properly.
